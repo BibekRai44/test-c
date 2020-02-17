437de6ec04eb2ac4e3d5b5de6bd28a367ee5487d
BibekRai44 <bibekrai655@gmail.com>
2020-02-17 11:06:55 +0545

    code changed

--- calculator.c
+++ calculator.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 main()
 {
-	printf("bibek\n");
+int a,b,c,d,e,f;
+printf("Enter first number ");
+scanf("%d",&a);
+printf("Enter second number");
+scanf("%d",&b);
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+printf("The result is %d %d %d %d",c,d,e,f);
 	getch();
 }
